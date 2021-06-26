e866fde2b9b0b1ec41c63f834f4d25fad9b73c18
Kubilay Tunçbaş <tuncbaskubilay@gmail.com>
2021-06-27 01:09:27 +0300

    lastdance

--- src/services/jobseekerService.js
+++ src/services/jobseekerService.js
@@ -4,4 +4,8 @@ export default class JobseekerService{
     getJobseekers(){
         return axios.get("http://localhost:8080/api/jobSeekers/getall");
     }
+
+    add(values){
+        return axios.post("http://localhost:8080/api/jobSeekers/add",values);
+    }
 }
